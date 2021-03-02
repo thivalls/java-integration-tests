1a5a1d9f09f56ea2516dbcd256a2a2b2849a9646
valls <olavalls@gmail.com>
2021-03-02 10:40:41 -0300

    Criando método de listagem de usuarios e testando

--- src/main/java/br/com/alura/leilao/dao/UsuarioDao.java
+++ src/main/java/br/com/alura/leilao/dao/UsuarioDao.java
@@ -2,11 +2,14 @@ package br.com.alura.leilao.dao;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import br.com.alura.leilao.model.Usuario;
+
+import java.util.List;
 
 @Repository
 public class UsuarioDao {
@@ -17,6 +20,11 @@ public class UsuarioDao {
 	@Autowired
 	UsuarioDao(EntityManager em) {
 		this.em = em;
+	}
+
+	public List<Usuario> all() {
+		Query $query = em.createQuery("select u from Usuario u");
+		return $query.getResultList();
 	}
 
 	public Usuario buscarPorUsername(String username) {
--- src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -2,31 +2,55 @@ package br.com.alura.leilao.dao;
 
 import br.com.alura.leilao.model.Usuario;
 import br.com.alura.leilao.util.JPAUtil;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
+import java.util.List;
 
 class UsuarioDaoTest {
     private UsuarioDao userDao;
+    private EntityManager em;
+
+    @BeforeEach
+    public void beforEach() {
+        this.em = JPAUtil.getEntityManager();
+        this.userDao = new UsuarioDao(em);
+        em.getTransaction().begin();
+    }
+
+    @AfterEach
+    public void afterEach() {
+        em.getTransaction().rollback();
+    }
 
     @Test
     void deveEncontrarUmUsuarioPeloNome() {
-        EntityManager em = JPAUtil.getEntityManager();
-        Usuario testUser = new Usuario("Fulano", "fulano@gmail.com", "$2a$10$8MeF8YTUTv22DVthkhOs3.WGT4W1Wp1xRXcRxTM12MgDzRviDpw7i");
-        em.getTransaction().begin();
-        em.persist(testUser);
-        em.getTransaction().commit();
-        this.userDao = new UsuarioDao(em);
+        createUserToTest("Fulano", "fulano@gmail.com", "$2a$10$8MeF8YTUTv22DVthkhOs3.WGT4W1Wp1xRXcRxTM12MgDzRviDpw7i");
         Usuario fulano = this.userDao.buscarPorUsername("Fulano");
         Assertions.assertNotNull(fulano);
     }
 
     @Test
     void naoDeveEncontrarUmUsuarioPeloNome() {
-        EntityManager em = JPAUtil.getEntityManager();
-        this.userDao = new UsuarioDao(em);
         Assertions.assertThrows(NoResultException.class, () -> this.userDao.buscarPorUsername("Fulano"));
+    }
+
+    @Test
+    void naoRetornarTodosUsuariosCadastrados() {
+        createUserToTest("fulano", "fulano@gmail.com", "password");
+        createUserToTest("ciclano", "ciclano@gmail.com", "password");
+        createUserToTest("beltrano", "beltrano@gmail.com", "password");
+
+        List<Usuario> users = this.userDao.all();
+        Assertions.assertEquals(3, users.size());
+    }
+
+    private void createUserToTest(String name, String email, String password) {
+        Usuario testUser = new Usuario(name, email, password);
+        em.persist(testUser);
     }
 }
